7cdaab334628185a7390643c97d4d8bd43f4a498
Mauroesco2022 <110541011+Mauroesco2022@users.noreply.github.com>
2023-05-08 23:57:54 -0500

    Contador
    Contador

--- Assets/Scripts/Options.cs
+++ Assets/Scripts/Options.cs
@@ -1,12 +1,26 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.UIElements;
+using System.Collections;
+using System.Collections.Generic;
 
 [System.Serializable]
 
-public class Options
+public class Options 
 {
+    [SerializeField] private float cantidadPuntos;
+    
+    [SerializeField] private Puntaje puntaje;
+
     public string text = null;
    
     public bool correct = false;
-}
+     
+    //private void sumarPunticos()
+    //{
+      //  if (correct == true)
+        //{
+         //   puntaje.SumarPuntos(cantidadPuntos);
+        //}
+    }
+
---
+++ Assets/Scripts/Puntaje.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class Puntaje : MonoBehaviour
+{
+    
+    public float timer = 0;
+    public TextMeshProUGUI textMesh;
+   
+    private void Start()
+    {
+        textMesh = GetComponent<TextMeshProUGUI>();
+         
+    }
+    private void Update()
+    {
+        timer -= Time.deltaTime;
+        if(timer <= 0f){
+        timer = 0f;
+        }
+        textMesh.text = "" + timer.ToString("0");
+        
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
